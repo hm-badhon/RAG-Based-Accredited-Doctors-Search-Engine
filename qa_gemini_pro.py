17688caeead19bda6f896ac72280ec7ffd02b2b4
Bahauddin Taha <tahaemon5@gmail.com>
2024-12-07 23:12:12 -0800

    Add files via upload

---
+++ qa_gemini_pro.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import time
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+import chromadb
+chromadb.api.client.SharedSystemClient.clear_system_cache()
+
+from dotenv import load_dotenv
+load_dotenv()
+
+st.title("RAG-Based Accredited Doctors Search Engine")
+loader = PyPDFLoader("doctors_list.pdf")
+data = loader.load()
+# print(data)
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000)
+docs = text_splitter.split_documents(data)
+# print(len(docs))
+
+# vectorstore = Chroma.from_documents(documents=docs, embedding=GoogleGenerativeAIEmbeddings(model="models/embedding-001"))
+vectorstore = Chroma.from_documents(documents=docs, embedding=GoogleGenerativeAIEmbeddings(model="models/embedding-001"))
+
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 10})
+
+llm = ChatGoogleGenerativeAI(model= "gemini-1.5-pro", temperature = 0, max_tokens=None, timeout=None)
+
+query = st.chat_input("Say Something: ")
+prompt = query
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+if query:
+    question_answer_chain = create_stuff_documents_chain(llm,prompt)
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+    response = rag_chain.invoke({"input": query})
+
+    formatted_response = response["answer"].replace('\n', '\n\n')  # Ensure double newlines for Markdown formatting
+    st.markdown(f"**Reply:**\n\n{formatted_response}")
+
+    # st.write(response["answer"])
